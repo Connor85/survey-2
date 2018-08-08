09d490ecad60e5e6da6c8ac5303fc64df904215f
Brian Nelson and Connor McCarthy <connor@>
2018-08-08 14:29:41 -0700

    add 3 survey forms and report results to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-  $("form#add").submit(function(){
-  })
+  $("form#survey").submit(function(event){
+    event.preventDefault();
+    var chip = $("input:radio[name=chip]:checked").val();
+    var food = $("#food").val();
+    var beverage = $("#beverage").val();
+    $("#output #rchip").text(chip);
+    $("#output #rfood").text(food);
+    $("#output #rbeverage").text(beverage);
+  });
 });
